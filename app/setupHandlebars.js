569dcdf2312e979ca09563832e713ef23c2878d1
chalmie <blairchalmers@gmail.com>
2016-07-07 13:10:18 -0700

    display formatted dates on dashboard

--- app/setupHandlebars.js
+++ app/setupHandlebars.js
@@ -1,4 +1,5 @@
-var ehandlebars = require('express-handlebars')
+var ehandlebars = require('express-handlebars'),
+    moment = require('moment')
 
 module.exports = function(app) {
   var hbs = ehandlebars.create({
@@ -8,6 +9,20 @@ module.exports = function(app) {
             return moment(date).format(format);
         },
       listEntries: function (entries) {
+        var unique = {};
+        var distinct = [];
+        for( var i in entries ){
+         if( typeof(unique[entries[i].day]) == "undefined"){
+          distinct.push(entries[i].day);
+         }
+         unique[entries[i].day] = 0;
+        }
+
+        var out = "<ul>";
+        for(var j=0, l=distinct.length; j<l; j++) {
+          out = out + "<li><a href='#'>" + moment(distinct[j]).format("MMM Do YYYY") + "</a></li>";
+        }
+        return out + "</ul>";
       },
       section: function(name, options) {
         if (!this._sections) this._sections = {}
